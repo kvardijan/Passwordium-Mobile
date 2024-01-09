507cf52f89bb4764ed40e84de4943ea3f7b9cdc4
Karlo Vardijan <kvardijan20@foi.hr>
2024-01-09 20:13:55 +0100

    Implementiran fetch request prijave na backend.

--- app/src/main/java/hr/foi/sis/passwordium/MainActivity.kt
+++ app/src/main/java/hr/foi/sis/passwordium/MainActivity.kt
@@ -3,10 +3,18 @@ package hr.foi.sis.passwordium
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
+import android.widget.EditText
+import hr.foi.sis.passwordium.models.User
+import hr.foi.sis.passwordium.models.UserResponse
+import hr.foi.sis.passwordium.network.NetworkServis
+import retrofit2.Call
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     private lateinit var btnPrijava: Button
+    private var userServis = NetworkServis.userServis
     private lateinit var testBtn: Button // FOR TESTING
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,6 +35,24 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun sendLoginRequest(){
+        val txtKorime = findViewById<EditText>(R.id.korimeTxt).text.toString()
+        val txtPassword = findViewById<EditText>(R.id.passwordTxt).text.toString()
+        val user = User(txtKorime, txtPassword)
 
+        userServis.login(user).enqueue(
+            object : retrofit2.Callback<UserResponse>{
+                override fun onResponse(
+                    call: Call<UserResponse>,
+                    response: Response<UserResponse>
+                ) {
+                    Log.i("Response", response.code().toString())
+                }
+
+                override fun onFailure(call: Call<UserResponse>, t: Throwable) {
+                    Log.i("Response", t.toString())
+                }
+
+            }
+        )
     }
 }
